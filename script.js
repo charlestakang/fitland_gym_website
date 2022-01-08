caf3c28c3723f60c9feada6cacac1da6cd42ecde
charlestakang <charles.takang@gmail.com>
2022-01-08 09:10:37 -0500

    class update as of jan 6

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ accordionBtns.forEach(accordion => {
     accordion.a
 })
 
-// accordionBtns.forEach(accordion => {
-//     accordion.addEventListener('click', ()=>{
-//         accordion.classList.toggle("is-open")
-//         let content = accordion.nextElementSibling;
-//         if(content.style.maxHeight){
-//             content.style.maxHeight = null;
-//         }else{
-//             content.style.maxHeight = content.scrollHeight + 'px'
-//         }
-//     })
-// })
+accordionBtns.forEach(accordion => {
+    accordion.addEventListener('click', ()=>{
+        accordion.classList.toggle("is-open")
+        let content = accordion.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + 'px'
+        }
+    })
+})
